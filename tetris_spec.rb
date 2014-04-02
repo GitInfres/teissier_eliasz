29aeea385cfd2e7fb5980ca2067b8d8eae8fe695
Laurent <laurent@pc-de-laurent.(none)>
2014-04-02 11:35:20 +0200

    Commit de la spec tetris

--- tetris_spec.rb
+++ tetris_spec.rb
@@ -2,10 +2,20 @@ require './tetris'
 
 
 describe Tetris, "#tourner" do
-  it "returns tourner à 90° OK" do
+  it "returns tourner à 90° droite OK" do
+    init_tab =[
+	 [0, 0, 1, 0],
+	 [0, 0, 1, 0],
+	 [0, 1, 1, 0],
+	 [0, 0, 0, 0]]
+    final_tab= [
+         [0, 0, 0, 0],
+         [0, 1, 0, 0],
+         [0, 1, 1, 1],
+         [0, 0, 0, 0]]
     tetris = Tetris.new
-    array =  bowling.tourner90(tableau)
-    tetris.checkout.should eq(array)
+    array =  tetris.tourner90droite(init_tab)
+    tetris.checkout(array).should eq(final_tab)
     
   end
 end
